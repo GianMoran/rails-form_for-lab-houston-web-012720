51e9bde6f88759e39b1369f29160f0e057bee890
GianMoran <gpmoran@uh.edu>
2020-02-25 09:40:36 -0600

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,29 @@
+class StudentsController < ApplicationController
+    def show
+        @student = current_student
+    end
+
+    def new 
+        @student = Student.new
+    end
+    
+    def edit
+        @student = current_student 
+    end
+    def create
+        @student = Student.create(student_params)
+        redirect_to student_path(@student)
+    end
+    def update 
+        @student = current_student
+        @student.update(student_params)
+        redirect_to student_path(@student)
+    end
+  private
+  def current_student
+    Student.find(params[:id])
+  end
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+end
